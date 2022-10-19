89952ad78e0c03c698c28e2bef4365be132627f6
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-10-19 17:41:27 +0700

    swaps the element at index x with the element at index y

--- Activity2/Program.cs
+++ Activity2/Program.cs
@@ -36,7 +36,21 @@
                 arr[1] = Int32.Parse(s1);
             }
         }
-        //swaps the
+        //swaps the element at index x with the element at index y
+        void swap(int x, int y)
+        {
+            int temp;
+
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
+        public void q_sort(int low, int high)
+        {
+            int pivot, i, j;
+            if (low > high)
+                return;
+        }
 
 
     }
