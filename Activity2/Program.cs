ca644ae0a5f7f05015cbcf866b9d12d4331b389a
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-10-19 17:37:05 +0700

    get array elements

--- Activity2/Program.cs
+++ Activity2/Program.cs
@@ -28,6 +28,16 @@
             Console.WriteLine("Enter Array Elements");
             Console.WriteLine("=======================");
 
+            //get array elements
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[1] = Int32.Parse(s1);
+            }
+        }
+        //swaps the
+
 
     }
 }
