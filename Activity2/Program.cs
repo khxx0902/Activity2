4750806e65771b8202a5ae2537caf18090dcb9d1
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-10-19 16:58:05 +0700

    menambahkan class program

--- Activity2/Program.cs
+++ Activity2/Program.cs
@@ -1,6 +1,15 @@
 ﻿namespace QuickSort
-{ 
+{
 
     class Program
-    { 
+    {
         //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
+        // Number of elements in array
+        private int n;
+    }
+}
+
