4a031e6a6703a4e67862e14d5ec74e8064a507ac
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-10-19 17:27:32 +0700

    menambahkan class program

--- Activity2/Program.cs
+++ Activity2/Program.cs
@@ -10,6 +10,18 @@
 
         // Number of elements in array
         private int n;
+
+
+        void input()
+        { 
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+
     }
 }
 
